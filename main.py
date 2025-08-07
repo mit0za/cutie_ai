436a1d9040ffe4a7b3730768268e63d576667835
mitoza <nightmitoza@gmail.com>
2025-08-07 20:31:47 +0930

    Update main.py to use chromadb to store index

--- main.py
+++ main.py
@@ -1,36 +1,29 @@
 from interfaces.llm_interface import set_llm
-from llama_index.core import Settings, SimpleDirectoryReader
-from llama_index.core import VectorStoreIndex, Settings
+from llama_index.core import Settings, StorageContext
 from llama_index.embeddings.ollama import OllamaEmbedding
+from llama_index.vector_stores.chroma import ChromaVectorStore
+from interfaces.index_interface import load_or_create_index
+import chromadb
 
-"""
-1.We first define our model because if we don't
-llamaindex will default to openAi
-"""
+# Set up LLM (ollama, llama2:7b)
 Settings.llm = set_llm(source="ollama", model="llama2:7b")
 
-# 2. We then define our embedding model
-Settings.embed_model = OllamaEmbedding(
-    model_name="nomic-embed-text:latest"
-)
+# Define our embedding model
+Settings.embed_model = OllamaEmbedding(model_name="nomic-embed-text:latest")
 
-# 3. We define where our data is so the AI will know where to look
-documents = SimpleDirectoryReader("./data").load_data()
+# Set up vector database
+chroma_client = chromadb.PersistentClient(path="./chroma_db") # For Prod
+chroma_collection = chroma_client.get_or_create_collection("index")
+vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
+index = load_or_create_index(vector_store, storage_context, data_path="./data")
 
-"""4. It then take our documents and split them into nodes. I know what you're thinking.
-Isn't that just indexing? Well yes, but also no. 
-"""
-vector_index = VectorStoreIndex.from_documents(documents)
-
-# 5. Make it searchable
-query_engine = vector_index.as_query_engine()
+# Make the index searchable
+query_engine = index.as_query_engine()
 
 # 6. We ask question
 resp = query_engine.query("What was her majesty please about?")
 
 # 7. We print answer
 print(resp)
-
-## 8.??
-## 9. Profit
