3df9976c6bf6f0dc823035b14a00160ac06271e5
mitoza <nightmitoza@gmail.com>
2025-08-12 19:20:07 +0930

    Update index_interface to check if collection is empty if not it will load existing ones

--- interfaces/index_interface.py
+++ interfaces/index_interface.py
@@ -1,12 +1,17 @@
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 
 def load_or_create_index(vector_store, storage_context, data_path="./data"):
-    """This check if vector exists"""
-    try:
-        index = VectorStoreIndex.from_vector_store(vector_store)
-        return index
-    except Exception:
-        """This will rebuild new index"""
-        documents = SimpleDirectoryReader(data_path, recursive=True).load_data()
-        print(f"Loaded {len(documents)} documents from {data_path}")
+    """Load an index from the vector store if it has data else build a new index."""
+
+    # Get how many collection in the db
+    get_collection = vector_store._collection.count()
+    
+    if get_collection == 0:
+        print(f"[Rebuild] Index empty. Rebuilding from {data_path} (this may take a while)...")
+        documents = SimpleDirectoryReader(data_path, recursive=True,).load_data()
+        print(f"[Rebuild] Loaded {len(documents)} documents from {data_path}")
         return VectorStoreIndex.from_documents(documents, storage_context=storage_context)
+    else:
+        print(f"[Load] Found {get_collection} in collection. Loading index...")
+        return VectorStoreIndex.from_vector_store(vector_store)
+
