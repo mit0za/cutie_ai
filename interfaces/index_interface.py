5fbce5a1f2740e7bc61f2c3e11843069861efe70
mitoza <nightmitoza@gmail.com>
2025-08-15 16:22:39 +0930

    Update index_interface to use big chunk size

--- interfaces/index_interface.py
+++ interfaces/index_interface.py
@@ -1,4 +1,5 @@
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.core.node_parser import SentenceSplitter
 
 def load_or_create_index(vector_store, storage_context, data_path="./data"):
     """Load an index from the vector store if it has data else build a new index."""
@@ -8,9 +9,17 @@ def load_or_create_index(vector_store, storage_context, data_path="./data"):
     
     if get_collection == 0:
         print(f"[Rebuild] Index empty. Rebuilding from {data_path} (this may take a while)...")
+
+        # Load raw docs
         documents = SimpleDirectoryReader(data_path, recursive=True,).load_data()
         print(f"[Rebuild] Loaded {len(documents)} documents from {data_path}")
-        return VectorStoreIndex.from_documents(documents, storage_context=storage_context)
+        
+        # Parse into nodes with custom chunking
+        parser = SentenceSplitter(chunk_size=1024, chunk_overlap=20, include_metadata=True)
+        nodes = parser.get_nodes_from_documents(documents)
+        print(f"[Rebuild] Prased into {len(nodes)} nodes")
+
+        return VectorStoreIndex.from_documents(nodes, storage_context=storage_context)
     else:
         print(f"[Load] Found {get_collection} in collection. Loading index...")
         return VectorStoreIndex.from_vector_store(vector_store)
