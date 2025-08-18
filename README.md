a391e43040ad2499ab6f305ac0e3945bc9a4c4a7
mitoza <nightmitoza@gmail.com>
2025-08-18 18:16:02 +0930

    Update main.py to cite source when answer questions

--- README.md
+++ README.md
@@ -11,9 +11,9 @@ pip install -r requirements.txt
 ollama pull llama3.1:8b
 ```
 
-**[Embed Model](https://ollama.com/mitoza/Qwen3-Embedding-0.6B)**
+**[Embed Model](https://ollama.com/dengcao/Qwen3-Embedding-0.6B)**
 ```
-ollama run mitoza/Qwen3-Embedding-0.6B
+ollama run dengcao/Qwen3-Embedding-0.6B
 ```
 
 **[Rerank model](https://ollama.com/linux6200/bge-reranker-v2-m3/tags)**
@@ -26,3 +26,7 @@ As of 11/08/25, Qwen3-Embedding-0.6B is the number 4 model on the Embedding Lead
 1. gemini-embedding-001 <br>
 2. Qwen3-Embedding-8B <br>
 3. Qwen3-Embedding-4B" <br>
+
+
+# Embedding 
+Took me 3h9m19s
--- main.py
+++ main.py
@@ -3,31 +3,42 @@ from llama_index.core import Settings, StorageContext
 from llama_index.embeddings.ollama import OllamaEmbedding
 from llama_index.vector_stores.chroma import ChromaVectorStore
 from interfaces.index_interface import load_or_create_index
+from llama_index.core.query_engine import CitationQueryEngine
 import chromadb
 
 # Set up LLM (ollama, llama2:7b)
 Settings.llm = set_llm(source="ollama", model="llama3.1:8b")
 
 # Define our embedding model
-Settings.embed_model = OllamaEmbedding(model_name="mitoza/Qwen3-Embedding-0.6B")
+# Settings.embed_model = OllamaEmbedding(model_name="dengcao/Qwen3-Embedding-0.6B:F16")
+Settings.embed_model = OllamaEmbedding(model_name="nomic-embed-text:latest")
 
 # Set up vector database
 chroma_client = chromadb.PersistentClient(path="./chroma_db") 
 chroma_collection = chroma_client.get_or_create_collection("index")
 vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
 storage_context = StorageContext.from_defaults(vector_store=vector_store)
-
 index = load_or_create_index(vector_store, storage_context, data_path="./data")
 
 # Make the index searchable
-query_engine = index.as_query_engine()
+# query_engine = index.as_query_engine(
+    # streaming=True,
+    # verbose=True,
+    # response_mode="tree_summarize",
+    # similarity_top_k = 10
+    # node_postprocessors=[rerank]
+# )
 
-# 6. We ask question
-# resp = query_engine.query("What was her majesty please about?")
-# resp = query_engine.query("What happended on the 6th of December 1841")
-# resp = query_engine.query("When was Jame McRisky born")
+query_engine = CitationQueryEngine.from_args(index, similarity_top_k=3, citation_chunk_size=512, streaming=True)
+# resp = index.as_retriever(similarity_top_k=20)
+# # node = resp.retrieve("What was Her Majesty pleased about?")
+# node = resp.retrieve("The new observatory that was built in Adelaide Univerity grounds. It is 13ft high with an additional 15ft for the dome. How much was it expected to cost?")
+# for i, node in enumerate(node):
+#     print(f"\n--- Chunk #{i+1} ---\n{node.text[:500]}")
+# print(node)
 
-# 7. We print answer
-while True:
-    resp = query_engine.query(input("Question:"))
-    print(f"Answer:\n{resp}")
+resp = query_engine.query("What do you think about FRIEND tv-shows")
+print(resp)
+print(resp.source_nodes[0].node.get_text())
+print(resp.source_nodes[1].node.get_text())
+print(resp.source_nodes[2].node.get_text())
