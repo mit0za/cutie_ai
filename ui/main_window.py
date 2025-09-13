30a318dcbb678cf3848343669279ab7ead5063f3
mitoza <nightmitoza@gmail.com>
2025-09-13 21:36:34 +0930

    Created get_engine method, handle_send method

--- ui/main_window.py
+++ ui/main_window.py
@@ -2,10 +2,12 @@ from PySide6.QtWidgets import (QApplication, QMainWindow, QTextBrowser,
                                QVBoxLayout, QWidget, QHBoxLayout)
 from ui.widget.chat_box import ChatBox
 from ui.widget.chat_display import ChatDisplay
+from backend.engine_manager import build_query_engine
 class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
+        self.query_engine = None
         self.setWindowTitle("Cutie AI")
 
         # Create container
@@ -23,11 +25,31 @@ class MainWindow(QMainWindow):
         main_layout.addWidget(right_widget)
 
         # Chat display
-        chat_display = ChatDisplay()
-        right_layout.addWidget(chat_display, stretch=1)
+        self.chat_display = ChatDisplay()
+        right_layout.addWidget(self.chat_display, stretch=1) # Add chat display
 
-        chat_box = ChatBox()
-        right_layout.addWidget(chat_box)
+        self.chat_box = ChatBox()
+        right_layout.addWidget(self.chat_box) # Add chat_box
+
+        self.chat_box.send_button.clicked.connect(self.handle_send)
+
+    def get_engine(self):
+        if self.query_engine is None:
+            self.query_engine = build_query_engine()
+        return self.query_engine
+    
+    def handle_send(self):
+        query = self.chat_box.input_widget.toPlainText().strip()
+        if not query:
+            return
+        self.chat_box.input_widget.clear()
+
+        engine = self.get_engine()
+
+        resp = engine.query(query)
+        self.chat_display.append(f"<b>Assistant:</b> {resp}")
+
+
 
 
 if __name__ == "__main__":
