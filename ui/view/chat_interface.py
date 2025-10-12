4c678fdccfd4e3e11e76c2e75d9977a6e81278df
mitoza <87408066+mit0za@users.noreply.github.com>
2025-10-13 07:12:52 +1030

    Update chat_interface to use textbrowser for displaying llm's responses

--- ui/view/chat_interface.py
+++ ui/view/chat_interface.py
@@ -1,13 +1,24 @@
-from qfluentwidgets import ScrollArea
-from PySide6.QtWidgets import QWidget
-
+from qfluentwidgets import ScrollArea, setTheme, Theme, TextBrowser
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout
 
 class ChatInterface(ScrollArea):
     """ Home interface """
 
     def __init__(self, parent=None):
         super().__init__(parent=parent)
-        self.view = QWidget(self)
-        self.setWindowTitle("Chat Interface")
-        self.view.setObjectName("view")
         self.setObjectName("chatInterface")
+        setTheme(Theme.DARK)
+
+        # layout setup
+        self.view = QWidget()
+        self.setWidget(self.view)
+        self.setWidgetResizable(True)
+        main_layout = QVBoxLayout(self.view)
+        main_layout.setContentsMargins(12, 12, 12, 12)
+        main_layout.setSpacing(8)
+
+        # chat display
+        self.chat_display = TextBrowser()
+        self.chat_display.setOpenExternalLinks(True)
+        self.chat_display.setReadOnly(True)
+        main_layout.addWidget(self.chat_display) # add to verticle layout
